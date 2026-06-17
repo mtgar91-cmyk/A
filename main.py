8b868979e0f3926be8f9ae7defa124a7b99c2b51
mtgar91-cmyk <mtgar91@gmail.com>
2026-06-18 02:54:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import time
 from scapy.all import IP, TCP, UDP, ICMP, send, raw
 from telebot import types
 
-TOKEN = "YOUR_ACTUAL_TOKEN"
-ADMIN_CHAT_ID = YOUR_ACTUAL_ID
+TOKEN = "7986030566:AAGVbAtYe_kkAOI3PdWIwfhvNL_dWHXQBAY"
+ADMIN_CHAT_ID = 6668195885
 MAX_PACKETS = 1000000  # Industrial-grade flood volume
 
 bot = telebot.TeleBot(TOKEN)
